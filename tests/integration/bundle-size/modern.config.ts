fbca731908c9128dd7c246590e43a910059feca0
chenjiahan <chenjiahan.jait@bytedance.com>
2023-03-24 16:53:25 +0800

    chore: add bundle-size test folder

---
+++ tests/integration/bundle-size/modern.config.ts
@@ -0,0 +1,14 @@
+import appTools, { defineConfig } from '@modern-js/app-tools';
+
+// https://modernjs.dev/docs/apis/app/config
+export default defineConfig({
+  runtime: {
+    router: true,
+  },
+  plugins: [appTools()],
+  performance: {
+    chunkSplit: {
+      strategy: 'all-in-one',
+    },
+  },
+});
